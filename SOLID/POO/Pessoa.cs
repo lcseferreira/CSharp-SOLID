160a2a922fc215be34e8485e6bd2d19fc9d887b0
Lucas <lcseferreira@hotmail.com>
2022-02-07 08:36:29 -0300

    Commit inicial + classes, objetos e construtores

---
+++ SOLID/POO/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿namespace SOLID.POO;
+
+public class Pessoa
+{
+    // FIELD = campo ou atributo
+    public string nome;
+    public int idade;
+    public string genero;
+
+    // CONSTRUCTOR = construtor
+    public Pessoa(string nome, int idade, string genero)
+    {
+        this.nome = nome;
+        this.idade = idade;
+        this.genero = genero;
+    }
+
+    // SOBRECARGA DE CONSTRUTOR
+    public Pessoa() { }
+
+    // METHOD = métodos
+    public void Identificar()
+    {
+        Console.WriteLine($"Olá, sou o {nome}, tenho {idade} anos e sou do sexo {genero}.");
+    }
+}
+
---
+++ SOLID/Program.cs
@@ -0,0 +1,23 @@
+﻿using SOLID.POO;
+
+namespace SOLID;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instânciando uma pessoa
+        Pessoa pessoa1 = new Pessoa();
+
+        // Acessando uma propriedade da classe
+        pessoa1.nome = "Lucas";
+        pessoa1.idade = 24;
+        pessoa1.genero = "Masculino";
+
+        // Instânciando uma pessoa com construtor
+        Pessoa pessoa2 = new Pessoa("Eduardo", 48, "Masculino");
+
+        pessoa1.Identificar();
+        pessoa2.Identificar();
+    }
+}
