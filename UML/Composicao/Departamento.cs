e15676f600e483cb60cfe71e06dd374ed83e0e01
Lucas <lcseferreira@hotmail.com>
2022-02-08 21:18:53 -0300

    UML - Agregação

--- UML/Composicao/Departamento.cs
+++ UML/Composicao/Departamento.cs
@@ -5,6 +5,9 @@
         private string _nome;
         private Escola escola;
 
+        // Relacionamento entre classes: Agregação => um departamento pode ter um ou mais professores
+        public IList<Professor> professores { get; set; }
+
         // Internal porque nosso departamento só existe dentro do contexto da Escola
         internal Departamento(Escola escola, string nome)
         {
---
+++ UML/Composicao/Professor.cs
@@ -0,0 +1,12 @@
+﻿namespace UML.Composicao
+{
+    public class Professor : IDisposable
+    {
+        public string Nome { get; set; }
+        public string Materia { get; set; }
+
+        public void Dispose()
+        {
+        }
+    }
+}
