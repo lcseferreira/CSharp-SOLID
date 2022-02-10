47c4a1742e8e9024897cb2014a2feac10b95329a
Lucas <lcseferreira@hotmail.com>
2022-02-10 17:36:14 -0300

    SOLID ISP - Interface Segregation Principle

---
+++ SOLID/ISP/CadastrarCliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.ISP
+{
+    internal class CadastrarCliente : IPersistencia, IMensagemEmail
+    {
+        public void EnviarEamil()
+        {
+            Console.WriteLine("Enviando email...");
+        }
+
+        public void SalvarDataBase()
+        {
+            Console.WriteLine("Salvando dados...");
+        }
+
+        public void ValidarDados()
+        {
+            Console.WriteLine("Validando dados...");
+        }
+    }
+}
---
+++ SOLID/ISP/CadastrarProduto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.ISP
+{
+    internal class CadastrarProduto : IPersistencia
+    {
+        public void SalvarDataBase()
+        {
+            Console.WriteLine("Salvando dados...");
+        }
+
+        public void ValidarDados()
+        {
+            Console.WriteLine("Validando dados...");
+        }
+    }
+}
