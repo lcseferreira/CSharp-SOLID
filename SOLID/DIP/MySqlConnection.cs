2023acf9bddb0deb1e8478c706402c3ba2c4b945
Lucas <lcseferreira@hotmail.com>
2022-02-10 17:56:02 -0300

    SOLID DIP - Dependency Inversion Principle

---
+++ SOLID/DIP/MySqlConnection.cs
@@ -0,0 +1,10 @@
+﻿namespace SOLID.DIP
+{
+    internal class MySqlConnection : IDataBaseConnection
+    {
+        public void Conectar()
+        {
+            Console.WriteLine("Conexão com o banco de dadaos...");
+        }
+    }
+}
---
+++ SOLID/DIP/RecuperarSenha.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.DIP
+{
+    internal class RecuperarSenha
+    {
+        // Módulos de alto nível (classes que dependem de outras classes) não devem depender de módulos de baixo nível. Ambos devem depender de uma abstração
+        // Injeção de dependência
+        private readonly IDataBaseConnection conexaoBanco;
+
+        public RecuperarSenha(IDataBaseConnection _conexao)
+        {
+            conexaoBanco = _conexao;
+        }
+    }
+}
