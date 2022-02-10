b31199752085a9a05ebf6fd1729e1af822cb8159
Lucas <lcseferreira@hotmail.com>
2022-02-10 18:15:07 -0300

    Exercício 5: Aplicando Princípios SOLID

---
+++ Exercicios/Exercicio5/GerDesc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.Exercicio5
+{
+    internal class GerDesc
+    {
+        public decimal Calcular(decimal valor, int tipo, int anos)
+        {
+            decimal resultado = 0;
+            decimal desc = (anos > 5) ? (decimal)5 / 100 : (decimal)anos / 100;
+
+            if (tipo == 1)
+            {
+                resultado = valor;
+            }
+            else if (tipo == 2)
+            {
+                resultado = (valor - (0.1M * valor)) - desc * (valor - (0.1M * valor));
+            }
+            else if (tipo == 3)
+            {
+                resultado = (0.7M * valor) - desc * (0.7M * valor);
+            }
+            else if (tipo == 4)
+            {
+                resultado = (valor - (0.5M * valor)) - desc * (valor - (0.5M * valor));
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ Exercicios/Exercicio5/GerenciadoDeDesconto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.Exercicio5
+{
+    internal class GerenciadorDeDesconto
+    {
+        public decimal AplicarDesconto(decimal precoProduto, int statusContaCliente, int tempoDaContaEmAnos)
+        {
+            decimal precoAposDesconto = 0;
+            decimal descontPorFidelidade = (tempoDaContaEmAnos > 5) ? (decimal)5 / 100 : (decimal)tempoDaContaEmAnos / 100;
+
+            if (statusContaCliente == 1)
+            {
+                precoAposDesconto = precoProduto;
+            }
+            else if (statusContaCliente == 2)
+            {
+                precoAposDesconto = (precoProduto - (0.1M * precoProduto)) - descontPorFidelidade * (precoProduto - (0.1M * precoProduto));
+            }
+            else if (statusContaCliente == 3)
+            {
+                precoAposDesconto = (0.7M * precoProduto) - descontPorFidelidade * (0.7M * precoProduto);
+            }
+            else if (statusContaCliente == 4)
+            {
+                precoAposDesconto = (precoProduto - (0.5M * precoProduto)) - descontPorFidelidade * (precoProduto - (0.5M * precoProduto));
+            }
+
+            return precoAposDesconto;
+        }
+    }
+}
