5bb60f7bab53266644ff00217c72a623a3bc9266
Lucas <lcseferreira@hotmail.com>
2022-02-10 18:31:37 -0300

    Exercício 5: adicionando constantes e ajustando o código

---
+++ Exercicios/Exercicio5/Constantes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.Exercicio5
+{
+    internal static class Constantes
+    {
+        public const int DESCONTO_MAXIMO_POR_FIDELIDADE = 5;
+        public const decimal DESCONTO_CLIENTE_COMUM = 0.1M;
+        public const decimal DESCONTO_CLIENTE_ESPECIAL = 0.3M;
+        public const decimal DESCONTO_CLIENTE_VIP = 0.5M;
+    }
+}
--- Exercicios/Exercicio5/GerenciadoDeDesconto.cs
+++ Exercicios/Exercicio5/GerenciadoDeDesconto.cs
@@ -11,7 +11,8 @@ namespace Exercicios.Exercicio5
         public decimal AplicarDesconto(decimal precoProduto, StatusContaCliente statusContaCliente, int tempoDaContaEmAnos)
         {
             decimal precoAposDesconto = 0;
-            decimal descontPorFidelidade = (tempoDaContaEmAnos > 5) ? (decimal)5 / 100 : (decimal)tempoDaContaEmAnos / 100;
+            decimal descontPorFidelidade = (tempoDaContaEmAnos > Constantes.DESCONTO_MAXIMO_POR_FIDELIDADE) ?
+                (decimal)Constantes.DESCONTO_MAXIMO_POR_FIDELIDADE / 100 : (decimal)tempoDaContaEmAnos / 100;
 
             switch (statusContaCliente)
             {
@@ -20,17 +21,17 @@ namespace Exercicios.Exercicio5
                     break;
 
                 case StatusContaCliente.ClienteComum:
-                    precoAposDesconto = (precoProduto - (0.1M * precoProduto));
+                    precoAposDesconto = (precoProduto - (Constantes.DESCONTO_CLIENTE_COMUM * precoProduto));
                     precoAposDesconto -= (descontPorFidelidade * precoAposDesconto);
                     break;
 
                 case StatusContaCliente.ClienteEspecial:
-                    precoAposDesconto = (precoProduto - (0.3M * precoProduto));
+                    precoAposDesconto = (precoProduto - (Constantes.DESCONTO_CLIENTE_ESPECIAL * precoProduto));
                     precoAposDesconto -= (descontPorFidelidade * precoAposDesconto);
                     break;
 
                 case StatusContaCliente.ClienteVIP:
-                    precoAposDesconto = (precoProduto - (0.5M * precoProduto));
+                    precoAposDesconto = (precoProduto - (Constantes.DESCONTO_CLIENTE_VIP * precoProduto));
                     precoAposDesconto -= (descontPorFidelidade * precoAposDesconto);
                     break;
 
